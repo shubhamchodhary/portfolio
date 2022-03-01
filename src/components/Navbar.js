7483f2f6c320d70e7c7fb4ce9e2226d215bfeb81
Shubham Panwar <shubhamcpanwar@outlook.com>
2022-03-01 23:24:03 +0530

    helloworld

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { FaAlignRight } from "react-icons/fa"
+import PageLinks from "../constants/links"
+const Navbar = () => {
+  return <h2>navbar component</h2>
+}
+
+export default Navbar
