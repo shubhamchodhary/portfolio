f4b81f8b83ec0e974c81337de6020050da6081ce
shubhamcpanwar <59318241+shubhamcpanwar@users.noreply.github.com>
2022-03-02 23:59:31 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import Image from "gatsby-image"
 // ...GatsbyImageSharpFluid
 const About = () => {
   return <h2>about page</h2>
-  <h3>fdgdfgd
+  <h3>Changes to about Page<h3>
 }
 
 export default About
